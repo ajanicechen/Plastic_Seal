37651e9f8ad584843f0c5d7d25bd783b24e536ca
vishaal010 <36966434+vishaal010@users.noreply.github.com>
2023-06-13 13:11:24 +0200

    Video Collider
    Commit

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -1,26 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Video;
-
 
 public class Collision : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
 
-    void Start()
-    {
-        // Get the VideoPlayer component from the same GameObject or another appropriate source
-        videoPlayer = GetComponent<VideoPlayer>();
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Seal")
         {
-            Debug.Log("ENTER");
-            // Play the video
-            videoPlayer.Play();
+            SceneManager.LoadScene("Cutscene1");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Seal")
+        {
+            print("STAY");
         }
     }
 
@@ -28,9 +28,31 @@ public class Collision : MonoBehaviour
     {
         if (other.gameObject.tag == "Seal")
         {
-            Debug.Log("EXIT");
-            // Stop the video
-            videoPlayer.Stop();
+            print("EXIT");
         }
+    }
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        StartCoroutine(LoadPlaygroundScene());
+    }
+
+    IEnumerator LoadPlaygroundScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Playground", LoadSceneMode.Single);
+
+        // Wait until the new scene is fully loaded
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Unload the Cutscene1 scene
+        SceneManager.UnloadSceneAsync("Cutscene1");
     }
 }
---
+++ Assets/Scenes/VideoPlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoPlayerController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoFinished;
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Playground");
+    }
+}
